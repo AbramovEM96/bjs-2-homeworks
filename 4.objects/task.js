4f4df32a84ddff3b600092516dac6b0ce7bca4b2
Egor Abramov <eg007or@inbox.ru>
2023-10-24 09:43:26 +0300

    4 задание

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,35 +1,32 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 };
 
-Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 };
 
-Student.prototype.addMarks = function (...marksToAdd) {
-    if (this.hasOwnProperty("marks")) {
-        this.marks.push(...marksToAdd);
-      }
+Student.prototype.addMarks = function(...marksToAdd) {
+  if (this.hasOwnProperty("marks")) {
+    this.marks.push(...marksToAdd);
+  }
 };
 
-Student.prototype.getAverage = function () {
-    let sum = 0;
-    if (this.marks && this.marks.length !== 0) {
-        for (let i = 0; i < this.marks.length; i++) {
-            sum += this.marks[i];
-        }
-        let average = sum / this.marks.length;
-        return average;
-    } else return 0
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  if (this.marks && this.marks.length !== 0) {
+    for (let i = 0; i < this.marks.length; i++) {
+      sum += this.marks[i];
+    }
+    return sum / this.marks.length;
+  } else return 0
 };
 
-
-
-Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
 };
