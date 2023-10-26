bb92e52c0c20ae95c65e8874e5a8fab375045b70
Egor Abramov <eg007or@inbox.ru>
2023-10-26 14:59:26 +0300

    massiv

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,7 @@ function compareArrays(arr1, arr2) {
     return arr1.length === arr2.length && arr1.every((el, i) => el === arr2[i]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
     const result = users.filter(user => user.gender === gender).map(user => user.age).reduce((total, age, index, array) => {
         if (array.length === 0) {
@@ -12,3 +13,4 @@ function getUsersNamesInAgeRange(users, gender) {
     }, 0)
     return result;
     };
+
